e43e4f1b01450e07c5f05cb4392f7da5f7e8761d
jniculescu <jani.niculescu@gmail.com>
2019-01-31 08:49:48 +0200

    PasswordHasher.cs created.

---
+++ BankApi/BankApi/Utilities/PasswordHasher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace BankApi.Utilities
+{
+    public class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            string salt = "maximit";
+
+            byte[] saltBytes = Encoding.ASCII.GetBytes(salt);
+
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: saltBytes,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+            return hashed;
+        }
+    }
+}
