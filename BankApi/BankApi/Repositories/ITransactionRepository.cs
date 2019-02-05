5312874904ef22f3fd8c6f6ac2e20e3101d06309
jniculescu <jani.niculescu@gmail.com>
2019-02-05 18:43:00 +0200

    ..

--- BankApi/BankApi/Repositories/ITransactionRepository.cs
+++ BankApi/BankApi/Repositories/ITransactionRepository.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BankApi.Models;
 
 namespace BankApi.Repositories
 {
     interface ITransactionRepository
     {
         TransactionRepository Create(Transaction transaction);
-        List<TransactionRepository> Read();
+        List<Transaction> Read(int IBAN);
     }
 }
