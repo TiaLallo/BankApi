cd8ecd1d41486fdf9c5047190dac8efa00954f77
TiaLallo <tia.lallo@gmail.com>
2019-02-15 12:30:22 +0200

    Startup

--- BankApi/BankApi/Startup.cs
+++ BankApi/BankApi/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using BankApi.Services;
+using BankApi.Repositories;
 
 namespace BankApi
 {
@@ -25,6 +27,7 @@ namespace BankApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
